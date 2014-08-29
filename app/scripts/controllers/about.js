6590b159ee3a61fca66f1b84e9a8eecd79a9681d
gitSauced <ttan08@gmail.com>
2014-08-28 20:00:22 -0700

    Yo generated

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lenderBeeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lenderBeeApp
+ */
+angular.module('lenderBeeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lenderBeeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lenderBeeApp
+ */
+angular.module('lenderBeeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
