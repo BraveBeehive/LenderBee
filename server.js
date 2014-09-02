84a018ae6ba2781f327305d0af63d5a0c5f75dd7
JonathanWarrick <jonathan.warrick@gmail.com>
2014-09-02 08:36:10 -0700

    Updated Express skeleton.

--- server.js
+++ server.js
@@ -6,10 +6,19 @@ var express = require('express');
 // Define app using Express
 var app = express();
 
+// Require morgan for request logging
+// https://github.com/expressjs/morgan
+var morgan = require('morgan');
+app.use(morgan('dev'));
+
 // Require bodyParser to handle JSON objects
+// https://github.com/expressjs/body-parser
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Set up static file-routing
+app.use(express.static(__dirname + '/app/client'));
 
 // Set up route-handling using Express 4.0 Router
 var router = express.Router();
@@ -26,18 +35,24 @@ router.get('/', function(request, response) {
 	response.json({ message: "this is a test" });
 });
 
-// route for adding new items to inventory
-router.route('/add')
+// routes for managing inventory
+router.route('/inventory')
   .post(function(request, response) {
   	console.log('adding a new item to user inventory');
   	// insert logic for database query
   })
+  .post(function(request, response) {
+  	console.log('removing an existing item from user inventory');
+  	// insert logic for database query 
+  });
+
 // route for searching for an item
+router.route('/search')
   .post(function(request, response) {
   	console.log('searching for an item');
   	// insert logic for database query
   });
-  
+
 // Set up routes to be prefixed with API
 app.use('/api', router);
 
@@ -45,3 +60,5 @@ app.use('/api', router);
 var port = process.env.PORT || 7432;
 app.listen(port);
 console.log('Server now open and listening on port:', port);
+
+// Connect to database
