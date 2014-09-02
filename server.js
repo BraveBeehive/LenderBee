d3037f85d273a90332ea17d3644b8d93584398ca
JonathanWarrick <jonathan.warrick@gmail.com>
2014-09-01 19:58:13 -0700

    Adds base logic for the server, including a test route and the skeleton for a few key database queries (to be filled-in by Tommy).

--- server.js
+++ server.js
@@ -11,15 +11,33 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 // Set up route-handling using Express 4.0 Router
 var router = express.Router();
 
-// base route for testing purposes
-router.get('/', function(req, res) {
-	res.json({ message: "this is a test" });
+// middleware to use for ALL requests to confirm that something is happening
+// strictly for testing purposes (to be removed before )
+router.use(function(request, response, next) {
+	console.log("A request has been made to the server");
+	next();
 });
 
+// base route for testing purposes
+router.get('/', function(request, response) {
+	response.json({ message: "this is a test" });
+});
+
+// route for adding new items to inventory
+router.route('/add')
+  .post(function(request, response) {
+  	console.log('adding a new item to user inventory');
+  	// insert logic for database query
+  })
+// route for searching for an item
+  .post(function(request, response) {
+  	console.log('searching for an item');
+  	// insert logic for database query
+  });
+  
 // Set up routes to be prefixed with API
 app.use('/api', router);
 
