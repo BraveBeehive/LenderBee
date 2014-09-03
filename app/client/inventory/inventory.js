aabdbf175d7c319b2b7317fc2d40165754bc65a4
Mike Luby <Michael@mLuby.com>
2014-09-03 08:48:50 -0700

    Users can add or borrow items, and conditionally lend, demand, and return items.

--- app/client/inventory/inventory.js
+++ app/client/inventory/inventory.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * @ngdoc function
  * @name lenderBeeApp.controller:MainCtrl
@@ -6,28 +8,48 @@
  * Controller of the inventory lenderBeeApp
  */
 
+var user = "Tommy";
+
 app
   .factory('Inventory', function(){
     // console.log('Inventory factory loaded!');
     return [
         {
-          name: "spork",
-          owner: "Tommy", 
-          possessor: "Tommy"
+          name: 'spork',
+          owner: 'Tommy', 
+          possessor: 'Tommy',
+          isRequested: true
+
         },
         {
-          name: "bike",
-          owner: "Tommy", 
-          possessor: "Collin"
+          name: 'bike',
+          owner: 'Tommy', 
+          possessor: 'Tommy',
+          isRequested: false
         },
         {
-          name: "headphones",
-          owner: "Tommy", 
-          possessor: "Jonathan"
+          name: 'headphones',
+          owner: 'Tommy', 
+          possessor: 'Jonathan',
+          isRequested: false
+        },
+        {
+          name: 'headphones',
+          owner: 'Collin', 
+          possessor: 'Jonathan',
+          isRequested: false
         }
       ];
   })
   .controller('InventoryCtrl', function($scope, Inventory){
     // console.log('InventoryCtrl loaded!');
     $scope.inventory = Inventory;
+    $scope.add = function(){console.log('add');};
+    $scope.borrow = function(){console.log('borrow');};
+    $scope.currentUser = "Tommy";
+  })
+  .controller('ItemCtrl', function($scope){
+    $scope.lend = function(){console.log('lend');};
+    $scope.return = function(){console.log('return');};
+    $scope.demand = function(){console.log('demand');};    
   });
