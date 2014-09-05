9b302257f0698ee98f4f5ebd36b3da8fceb51e97
Collin <collinwu@gmail.com>
2014-09-04 22:20:24 -0700

    Synced server.js and routes.js before latest pull

--- app/server/config/routes.js
+++ app/server/config/routes.js
@@ -1,42 +1,42 @@
 'use strict';
 
-// var util = require('../database/utility.js');
+var util = require('../database/utility.js');
 
 module.exports = function(app, passport) {
-	
-	app.use(function(request, response, next) {
-		console.log('A request has been made to the server');
-		next();
-	});
+  
+  app.use(function(request, response, next) {
+    console.log('A request has been made to the server');
+    next();
+  });
 
-	// base route for testing purposes
-	// app.get('/', function(request, response) {
-	// 	response.json({ message: 'this is a test' });
-	// });
+  // base route for testing purposes
+  // app.get('/', function(request, response) {
+  //  response.json({ message: 'this is a test' });
+  // });
 
-	// routing for searchbar
-	app.post('/api/search', function(request, response) {
-		console.log(request.body);
-		console.log('searching for an item');
-  	// insert logic for database query
-  	// util.searchForItemInInventory(request, response);
-		// response.send(200); // Response to be handled by utility function
-	});
+  // routing for searchbar
+  app.post('/api/search', function(request, response) {
+    console.log(request.body);
+    console.log('searching for an item');
+    // insert logic for database query
+    util.searchForItemInInventory(request, response);
+    // response.send(200); // Response to be handled by utility function
+  });
 
-	// routes for managing inventory
-	app.post('/inventory', function(request, response) {
-  	console.log('adding a new item to user inventory');
-  	// insert logic for database query
-  	// util.addItemToInventory();
-  	response.send(200);
+  // routes for managing inventory
+  app.post('/inventory', function(request, response) {
+    console.log('adding a new item to user inventory');
+    // insert logic for database query
+    // util.addItemToInventory();
+    response.send(200);
   });
 
   // remove item from inventory
   app.post('/remove', function(request, response) {
-  	console.log('removing an existing item from user inventory');
-  	// insert logic for database query
-  	// util.removeItemFromInventory();
-  	response.send(200);
+    console.log('removing an existing item from user inventory');
+    // insert logic for database query
+    // util.removeItemFromInventory();
+    response.send(200);
   });
 
   // for facebook login/authentication
@@ -44,24 +44,24 @@ module.exports = function(app, passport) {
 
   // handle the callback after facebook has verified the user
   app.get('/auth/facebook/callback', passport.authenticate('facebook'), function(request, response) {
-  	console.log('user authenticated via facebook');
-  	response.send(200);
+    console.log('user authenticated via facebook');
+    response.send(200);
   });
 
   // handle logout/session end
   app.get('/logout', function(request, response) {
-  	console.log('user is logging out');
-  	request.logout();
-  	response.send(200);
+    console.log('user is logging out');
+    request.logout();
+    response.send(200);
   });
 };
 
 // Utility functions:
 var isLoggedIn = function(request, response, next) {
-	if (request.isAuthenticated()) {
-		return next();
-	}
+  if (request.isAuthenticated()) {
+    return next();
+  }
 
-	console.log('not logged in');
-	response.send(200);
+  console.log('not logged in');
+  response.send(200);
 };
